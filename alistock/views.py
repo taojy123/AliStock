06e7cff8be32b7cb41594912dddf8e3d4b1a1581
taojy123 <taojy123@163.com>
2016-04-14 14:12:24 +0800

    fix

--- alistock/views.py
+++ alistock/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Sum
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import auth
